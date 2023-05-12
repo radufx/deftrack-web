c1cef77cde08f9d1faa1af241ad2ad1b8a8bea32
radufx <raduftw01@gmail.com>
2023-05-12 23:17:26 +0300

    feat: added initial dependencies

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 130,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
